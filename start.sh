dd78dfdd7667a367685bf4547aa02b7c81a38d51
Breath Swag <bswag@oa-a4.msk.21-school.ru>
2021-01-19 16:38:09 +0300

    change start.sh for easy starting (flag -s)

--- start.sh
+++ start.sh
@@ -4,13 +4,23 @@ vm_name="default"
 img_name="bswag_server"
 
 case "$1" in
-	-fs) bash ~/Desktop/projects/clearcache.sh
+	-s) bash ./start.sh -rm
+		bash ./start.sh -mc
+		docker-machine stop $vm_name
+		bash ./start.sh -fs
+		. ./start.sh -ms
+		. ./start.sh -b
+		. ./start.sh -c ;;
+	-fs)bash ./scripts/clearcache.sh
 		mv ~/.docker/machine ~/goinfre 
 		ln -s ~/goinfre/machine ~/.docker/machine ;;
-	-mc) docker-machine create $vm_name
+	-rm)docker-machine rm $vm_name
+		rm -rf ~/.docker/machine ~/goinfre/machine ;;
+	-mc)docker-machine create $vm_name
 		eval $(docker-machine env $vm_name) ;;
-	-ms) docker-machine start $vm_name
-		eval $(docker-machine env $vm_name) ;;
+	-ms)docker-machine start $vm_name
+		eval $(docker-machine env $vm_name)
+		env | grep DOCKER ;;
 	-b) docker build --tag $img_name . ;;
 	-c) ip=$(docker-machine ip $vm_name)
 		docker run -it -e IP_VM=$ip -p 80:80 -p 443:443 --rm $img_name ;;
